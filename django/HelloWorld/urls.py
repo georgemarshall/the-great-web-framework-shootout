bcb7472401a310da848b60292f389bd173598f1c
George Marshall <george@socialups.com>
2012-08-28 16:06:41 -0700

    urls.py cleanup

--- django/HelloWorld/urls.py
+++ django/HelloWorld/urls.py
@@ -4,18 +4,8 @@ from django.conf.urls.defaults import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'HelloWorld.views.home', name='home'),
-    # url(r'^HelloWorld/', include('HelloWorld.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    
-    (r'^hello$', 'views.index'),
-    (r'^dtl_hello$', 'views.dtl_hello'),
-    (r'^dtl_sql$', 'views.dtl_sql'),
+urlpatterns = patterns('views',
+    url(r'^hello$', 'index'),
+    url(r'^dtl_hello$', 'dtl_hello'),
+    url(r'^dtl_sql$', 'dtl_sql'),
 )
